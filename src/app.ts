b3cb0630ca136056e63e24b0ae151711b7ebac47
Anup-Naik <anupnaik54810@gmail.com>
2025-09-17 23:29:55 +0530

    Reapply "Health-Check 🏥"
    This reverts commit 940bfef217490b9917c1531314a0b88092c4f7f0.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
+import type { Express } from 'express';
 import express from 'express';
 
-export function buildApp() {
+import { apiV1Router } from './routes.js';
+
+export function buildApp(): Express {
   const app = express();
 
   // Middleware for JSON parsing.
   app.use(express.json());
+
+  // /api/v1/ routes
+  app.use('/api/v1', apiV1Router);
 
   return app;
 }
