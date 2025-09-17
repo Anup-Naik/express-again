f8d822572eb3b46476caf2f6af050f5e46bbee49
Anup-Naik <anupnaik54810@gmail.com>
2025-09-17 17:50:40 +0530

    Initial Commit🔰

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+// prettier.config.js
+export default {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  plugins: [],
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
