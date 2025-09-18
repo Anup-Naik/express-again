4f163d6718562074ccd32a68e3272f64eecfc919
Anup-Naik <anupnaik54810@gmail.com>
2025-09-18 19:37:21 +0530

    prisma with postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import type { Express } from 'express';
 import express from 'express';
 
@@ -6,8 +7,9 @@ import { apiV1Router } from './routes.js';
 export function buildApp(): Express {
   const app = express();
 
-  // Middleware for JSON parsing.
+  // Middleware
   app.use(express.json());
+  app.use(cookieParser());
 
   // /api/v1/ routes
   app.use('/api/v1', apiV1Router);
